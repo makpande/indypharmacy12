b180bdd0b777d965eb853146507f7045e1fdb154
Makarand Pande <mak_pande@yahoo.com>
2016-01-08 10:10:14 -0500

    minor image changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :topic
   has_many :comments
-
+  has_ancestry
   def to_s
     title
   end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,8 +1,5 @@
 class Topic < ActiveRecord::Base
   has_many :posts
   mount_uploader :topicimage, TopicImageUploader
-
-  def to_s
-    title
-  end
+  
 end
